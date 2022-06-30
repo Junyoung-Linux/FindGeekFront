73d707be80c11da640018b4109f0fb32d209d8b9
Junyoung-Linux <jun@linuxgeek.io>
2022-06-30 14:05:35 +0900

    frontwork

---
+++ reactapp/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import CreateAccount from './CreateAccount';
+
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+          <CreateAccount/>
+      </div>
+  );
+  }
+}
+
+export default App;
